4d554d81ce78f39e7754fd1d77af045bf6505f12
Dennis Yildirim <dennis@yld.im>
2024-02-02 14:58:37 +0100

    Fix H1.2 first half

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -6,8 +6,8 @@ import fopbot.World;
 
 public class Main {
     public static void main(String[] args) {
-        World.setVisible(true);
         World.setSize(9, 9);
         Robot myRobot = new Robot(4, 4, Direction.UP, 38);
+        World.setVisible(true);
     }
 }
