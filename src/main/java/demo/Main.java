f5ff3498d5bc6660d1e869c352b8b8f2487d6eb7
Dennis Yildirim <dennis@yld.im>
2024-02-02 14:38:46 +0100

    1.2 First half

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,10 +1,13 @@
 package demo;
 
+import fopbot.Direction;
+import fopbot.Robot;
 import fopbot.World;
 
 public class Main {
     public static void main(String[] args) {
         World.setVisible(true);
-        System.out.println("Hello World");
+        World.setSize(9, 9);
+        Robot myRobot = new Robot(4, 4, Direction.UP, 38);
     }
 }
